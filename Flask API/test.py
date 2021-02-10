35581956f5503f09c89f3205f2d639efc0a60f43
Steven Yan <ssyan@ualberta.ca>
2021-02-09 19:24:21 -0700

    learning flask tutorial

--- Flask API/test.py
+++ Flask API/test.py
@@ -2,5 +2,18 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
+data = [{"likes": 78, "name": "Joe", "views": 13},
+        {"likes": 10, "name": "Steven", "views": 44},
+        {"likes": 123, "name": "Bob", "views": 53}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "video/0")
+print(response)
+
+input()
+
+response = requests.get(BASE + "video/2")
 print(response.json())
