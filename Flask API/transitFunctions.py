afa0ade38c9ad2eb75187748447b2de4a85663e4
Steven Yan <ssyan@ualberta.ca>
2021-02-13 22:56:13 -0700

    got time of all bus arrival/departures

--- Flask API/transitFunctions.py
+++ Flask API/transitFunctions.py
@@ -15,25 +15,32 @@ def get_stop_times(address):
     live_stop_feed = gtfs_realtime_pb2.FeedMessage()
     live_stop_response = requests.get(live_stop_url)
     live_stop_feed.ParseFromString(live_stop_response.content)
-
-    for entity in live_stop_feed.entity:
-        for each in entity.trip_update.stop_time_update:
-            if each.stop_id == "9174":
-                print(entity.trip_update.trip.route_id)
-                print(each)
+    for stop in closest_stops:
+        print(stop["stop_id"], stop["stop_name"])
+        for entity in live_stop_feed.entity:
+            for each in entity.trip_update.stop_time_update:
+                if each.stop_id == stop["stop_id"]:
+                    print(entity.trip_update.trip.route_id)
+                    time = each.departure.time
+                    if not time:
+                        time = each.arrival.time
+                    print(time)
 
 
 def find_closest_stops(address):
     response = requests.get(
         'https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false&key=AIzaSyCojSgnjODZ71ywFILooMge4RtrVeDh2TQ')
 
-    latLongResponse = response.json()
-    latLongAddress = latLongResponse['results'][0]['geometry']['location']
+    lat_long_response = response.json()
+    lat_long_address = lat_long_response['results'][0]['geometry']['location']
     static_stop_url = "https://data.edmonton.ca/resource/kgzg-mxv6.json?$limit=10000"
     static_stop_response = requests.get(static_stop_url)
     static_stop_data = static_stop_response.json()
-    static_stop_data = sorted(static_stop_data, key=lambda d: distance(d["stop_lat"], d["stop_lon"], latLongAddress['lat'], latLongAddress['lng']))
+    static_stop_data = sorted(static_stop_data, key=lambda d: distance(d["stop_lat"], d["stop_lon"], lat_long_address['lat'], lat_long_address['lng']))
     return static_stop_data[:3]
+
+def abc():
+    bus_schedule = "https://data.edmonton.ca/resource/greh-g7ac.json"
 
 def distance(lon1, lat1, lon2, lat2):
     R = 6371000  # radius of the Earth in m
@@ -45,5 +52,6 @@ def distance(lon1, lat1, lon2, lat2):
     x = (lon2 - lon1) * cos(0.5*(lat2+lat1))
     y = (lat2 - lat1)
     return R * sqrt(x*x + y*y)
+
 
 get_stop_times("11713 17 Ave. SW Edmonton, Alberta, Canada")
