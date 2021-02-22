04947585bfee479f10f8d05f960eba53e9642906
Steven Yan <ssyan@ualberta.ca>
2021-02-22 01:00:08 -0700

    Update searchBar.js

--- React Frontend/yegger-stop/src/components/searchBar.js
+++ React Frontend/yegger-stop/src/components/searchBar.js
@@ -1,10 +1,73 @@
-import React from 'react';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import React, { Component } from 'react';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
 
-const SearchBar = () => (
-    <div>
-        <GooglePlacesAutocomplete />
-    </div>
-);
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { address: '' };
+    }
+
+    handleChange = address => {
+        this.setState({ address });
+    };
+
+    handleSelect = address => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => console.log('Success', latLng))
+            .catch(error => console.error('Error', error));
+    };
+
+    render() {
+        return (
+            <Container>
+                <PlacesAutocomplete
+                    value={this.state.address}
+                    onChange={this.handleChange}
+                    onSelect={this.handleSelect}
+                >
+                    {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                        <Row>
+                            <input
+                                {...getInputProps({
+                                    placeholder: 'Enter Address',
+                                    className: 'location-search-input',
+                                })}
+                            />
+                            <Container className="autocomplete-dropdown-container"> 
+                                {loading && <div>Loading...</div>}
+                                {suggestions.map(suggestion => {
+                                    const className = suggestion.active
+                                        ? 'suggestion-item--active'
+                                        : 'suggestion-item';
+                                    // inline style for demonstration purpose
+                                    const style = suggestion.active
+                                        ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                        : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                    return (
+                                        <Row className="input_suggestion"
+                                            {...getSuggestionItemProps(suggestion, {
+                                                style,
+                                            })}
+                                        >
+                                            <span>{suggestion.description}</span>
+                                        </Row>
+                                    );
+                                })}
+                            </Container>
+                        </Row>
+                    )}
+                </PlacesAutocomplete>
+            </Container>
+        );
+    }
+}
 
 export default SearchBar;
