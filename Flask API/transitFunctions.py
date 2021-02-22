3720b8dbce8f345402f1e68591a7ceab566a2ae4
Steven Yan <ssyan@ualberta.ca>
2021-02-22 13:02:52 -0700

    finished transitFunctions.py

--- Flask API/transitFunctions.py
+++ Flask API/transitFunctions.py
@@ -1,14 +1,16 @@
 import requests
 import time
-import datetime
+from datetime import datetime
 import pytz
 from math import cos, sqrt
 from google.transit import gtfs_realtime_pb2
-import timeit
 
 def get_data(address):
     stop_results = {}
     closest_stops = find_closest_stops(address)
+
+    with open('routes.txt', 'r') as inf:
+        route_dict = eval(inf.read())
 
     live_stop_url = "http://gtfs.edmonton.ca/TMGTFSRealTimeWebService/TripUpdate/TripUpdates.pb"
     live_stop_feed = gtfs_realtime_pb2.FeedMessage()
@@ -21,14 +23,20 @@ def get_data(address):
         bus_times = []
         one_stop_result["stop_id"] = stop['stop_id']
         one_stop_result["stop_name"] = stop["stop_name"]
-
+        one_stop_result["lat"] = float(stop["stop_lat"])
+        one_stop_result["long"] = float(stop["stop_lon"])
+        one_stop_result["available_routes"] = route_dict[stop['stop_id']]
+        
         for entity in live_stop_feed.entity:
             for each in entity.trip_update.stop_time_update:
                 if each.stop_id == stop["stop_id"]:
                     time = each.departure.time
                     if not time:
                         time = each.arrival.time
-                    bus_times.append([entity.trip_update.trip.route_id, time])
+                    
+                    timezone = pytz.timezone("Canada/Mountain")
+                    read_time = datetime.fromtimestamp(int(time), timezone).strftime("%H:%M")
+                    bus_times.append([entity.trip_update.trip.route_id, read_time])
             one_stop_result["live_bus_times"] = bus_times
         
         stop_results["stop_" + str(i)] = one_stop_result
@@ -36,7 +44,12 @@ def get_data(address):
     
     return stop_results
 
+
 def find_routes():
+    # function that allows me to update and see which bus routes are available at bus stops
+    # Saves that data in a text file called routes.txt for faster access
+    # Data saved in the file is in the format of:  {stop_id: [route_ids], stop_id: [route_ids]}
+
     all_trips_url = "https://data.edmonton.ca/resource/ctwr-tvrd.json?$limit=50000"
     all_trips_response = requests.get(all_trips_url)
     all_trips_data = all_trips_response.json()
@@ -47,7 +60,6 @@ def find_routes():
 
     trip_dict = {}
 
-    i = 0
     for time in static_time_data:
         if time["stop_id"] in trip_dict:
             for data in all_trips_data:
@@ -57,8 +69,6 @@ def find_routes():
             for data in all_trips_data:
                 if time["trip_id"] == data["trip_id"]:
                     trip_dict[time["stop_id"]] = [data["route_id"]]
-        print(i)
-        i += 1
     
     for each in trip_dict:
         trip_dict[each] = list(set(trip_dict[each]))
@@ -68,6 +78,10 @@ def find_routes():
 
 
 def find_closest_stops(address):
+    # find the closest three bus stops to an address
+    # First switch the address to (lat, long) using Google Map Geocode API
+    # Then traverse through all stops in Edmonton to find the closest to the given (lat, long)
+
     response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&sensor=false&key=AIzaSyCojSgnjODZ71ywFILooMge4RtrVeDh2TQ')
     lat_long_response = response.json()
     lat_long_address = lat_long_response['results'][0]['geometry']['location']
@@ -79,6 +93,8 @@ def find_closest_stops(address):
     return static_stop_data[:3]
 
 def distance(lon1, lat1, lon2, lat2):
+    # distance formula that calculates between two (lat, long) positions
+    
     R = 6371000
     lon1 = float(lon1)
     lat1 = float(lat1)
@@ -89,12 +105,5 @@ def distance(lon1, lat1, lon2, lat2):
     y = (lat2 - lat1)
     return R * sqrt(x*x + y*y)
 
-# a = get_data("11713 17 Ave. SW Edmonton, Alberta, Canada")
-# print(a)
-
-
-start = timeit.default_timer()
-find_routes()
-stop = timeit.default_timer()
-
-print('Time: ', stop - start)
+a = get_data("11713 17 Ave. SW Edmonton, Alberta, Canada")
+print(a)
