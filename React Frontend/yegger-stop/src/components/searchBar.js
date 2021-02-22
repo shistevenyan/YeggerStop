116a65fc0771416c62f45db4b06f06b38bdedb48
Steven Yan <ssyan@ualberta.ca>
2021-02-22 00:43:16 -0700

    added autocomplete to searchbar

---
+++ React Frontend/yegger-stop/src/components/searchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
+const SearchBar = () => (
+    <div>
+        <GooglePlacesAutocomplete />
+    </div>
+);
+
+export default SearchBar;
